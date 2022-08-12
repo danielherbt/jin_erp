321c27edb652271a7292fc67c97d70bd920da2b2
daniel <danielherbt@yahoo.es>
2022-08-11 22:05:08 -0500

    make it better3

--- jin/asgi.py
+++ jin/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jin-erp.settings')
 
 application = get_asgi_application()
