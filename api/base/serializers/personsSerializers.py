0980e572ce56c806013beaf1d3de6581434cee44
danielherbt <danielherbt@yahoo.es>
2022-12-13 16:01:08 -0500

    init proyecto en github

--- api/base/serializers/personsSerializers.py
+++ api/base/serializers/personsSerializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from api.sales.models import Client,Vendor
 from api.schoolfees.models import Student
-from api.manage.models import UsersDb
+from api.manage.models import UsersDb, Company, User, ConnectDb
 
 ###----------- Sales---------------------------------
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
@@ -23,8 +23,24 @@ class StudentSerializer(serializers.HyperlinkedModelSerializer):
         exclude = ('status','created','updated') 
 
 ###----------- Manage ---------------------------------
-class UserDbSerializer(serializers.HyperlinkedModelSerializer):
+class UserDbSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsersDb
-        #fields = '__all__'
-        exclude = ('status','created','updated') 
+        fields = '__all__'
+        #exclude = ('status','created','updated') 
+
+    """ def to_representation(self,instance):
+        data = super().to_representation(instance)
+        
+        return {
+                'url' : data['url'],
+                'id'     : instance.id,
+                'Nombre': instance.name,
+                'Base de Datos' : instance.code,
+                'Compañia' : instance.company.name,
+                'Compañia Url': data['company'],
+                'Usuario' : instance.user.name,
+                'Usuario Url': data['user'],
+                'Conexion' : instance.connect.name,
+                'Conexion Url': data['connect']
+        } """  
--- api/manage/urls.py
+++ api/manage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include,path
 from rest_framework.routers import DefaultRouter
 from api.manage import views
-from api.manage.views import ConnectDbViewSet, ModulesViewSet
+#from api.manage.views import ConnectDbViewSet, ModulesViewSet, UserDbViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -13,8 +13,9 @@ router.register(r'subparroquia', views.SubDistrictViewSet)
 router.register(r'compania', views.CompanyViewSet)
 router.register(r'sucursal', views.SubsidiaryViewSet)
 router.register(r'genero',views.GenreViewSet)
-router.register(r'conexion-bd', ConnectDbViewSet)
-router.register(r'modulo', ModulesViewSet)
+router.register(r'conexion-bd', views.ConnectDbViewSet)
+router.register(r'modulo', views.ModulesViewSet)
+router.register(r'usuario-bd', views.UserDbViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
